6eae1496f0c174c81f78ee72e87ecc11774c812d
VerschaerenJeroen <r0678204@student.thomasmore.be>
2021-02-16 16:52:46 +0100

    Added pay With Calculations Added venuedetails html

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -4,9 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Controller
 public class HomeController {
     private final int mySpecialNumber = 729;
+
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    LocalDateTime now = LocalDateTime.now();
+    LocalDateTime payDay = now.plusDays(30);
+
+    String dateTimeString1 = now.format(formatter);
+    String dateTimeString2 = payDay.format(formatter);
 
     @GetMapping("/")
     public String home(Model model) {
@@ -19,7 +29,9 @@ public class HomeController {
         return "about";
     }
     @GetMapping("/pay")
-    public String pay() {
+    public String pay(Model model) {
+        model.addAttribute("dateNow", dateTimeString1);
+        model.addAttribute("payDay", dateTimeString2);
         return "pay";
     }
 }
