070810d6ed1c43f5b10968f75ac1d7b674284729
VerschaerenJeroen <verschaerenjeroen@gmail.com>
2021-02-21 22:22:02 +0100

    Finished Project

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -1,16 +1,20 @@
 package be.thomasmore.party.controllers;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Controller
 public class HomeController {
-    private final int mySpecialNumber = 729;
+    private final String[] venueNames = {"De Loods", "De Club", "De Hangar", "Zapoi", "Kuub", "Cuba Libre"};
+
+    private final String dayToday = LocalDate.now().format(DateTimeFormatter.ofPattern("E"));
 
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
     LocalDateTime now = LocalDateTime.now();
@@ -21,27 +25,37 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("mySpecialNumber", mySpecialNumber);
         return "home";
     }
+
     @GetMapping("/about")
     public String about(Model model) {
-        model.addAttribute("mySpecialNumber", mySpecialNumber);
         return "about";
     }
+
     @GetMapping("/pay")
     public String pay(Model model) {
+        model.addAttribute("dayToday", dayToday);
         model.addAttribute("dateNow", dateTimeString1);
         model.addAttribute("payDay", dateTimeString2);
         return "pay";
     }
-    @GetMapping({"/venuedetails", "/venuedetails/{venueName}"})
-    public String venueDetails(Model model, @PathVariable(required = false) String venueName) {
-        model.addAttribute("venueName", (venueName!=null) ? venueName : "--no venue chosen--");
-        return "venuedetails";
-    }
+
     @GetMapping("/venuelist")
     public String venuelist(Model model) {
+        model.addAttribute("venueNames", venueNames);
         return "venuelist";
+    }
+
+    @GetMapping({"/venuedetails", "venuedetails/{pathVariable}"})
+    public String venueDetails(Model model, @PathVariable(required = false) Integer pathVariable) {
+        if (pathVariable == null) {
+            pathVariable = -1;
+        }
+        if (pathVariable < venueNames.length && pathVariable >= 0) {
+            model.addAttribute("venueNames", venueNames[pathVariable]);
+        }
+
+        return "venuedetails";
     }
 }
