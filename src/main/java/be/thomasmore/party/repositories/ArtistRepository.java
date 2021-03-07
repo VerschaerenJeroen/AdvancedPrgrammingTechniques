5373413ac1fdfffbd3ebe7ed34fa4df275f17bc9
VerschaerenJeroen <verschaerenjeroen@gmail.com>
2021-03-07 16:07:50 +0100

    Updated to latest version

--- src/main/java/be/thomasmore/party/repositories/ArtistRepository.java
+++ src/main/java/be/thomasmore/party/repositories/ArtistRepository.java
@@ -1,16 +1,8 @@
 package be.thomasmore.party.repositories;
 
 import be.thomasmore.party.model.Artist;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface ArtistRepository extends CrudRepository<Artist,Integer> {
-    @Query("select a from Artist a where upper(a.artistName) like upper(concat('%',?1,'%'))  or" +
-            " upper(a.bio) like upper(concat('%',?1,'%')) or" +
-            " upper(a.genre) like upper(concat('%',?1,'%')) or" +
-            " upper(a.portfolio) like upper(concat('%',?1,'%'))")
-    List<Artist> findArtistsByArtistNameContainingQuery(String name);
 }
 
