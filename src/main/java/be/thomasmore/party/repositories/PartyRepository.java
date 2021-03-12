77b6e199feb4fd630d14361de7d1fbec9f84d31c
VerschaerenJeroen <verschaerenjeroen@gmail.com>
2021-03-12 10:09:19 +0100

    Added party to data.sql

---
+++ src/main/java/be/thomasmore/party/repositories/PartyRepository.java
@@ -0,0 +1,8 @@
+package be.thomasmore.party.repositories;
+
+import be.thomasmore.party.model.Party;
+import be.thomasmore.party.model.Venue;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PartyRepository extends CrudRepository<Party, Integer> {
+}
